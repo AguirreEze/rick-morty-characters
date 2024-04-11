e33c2f2444289c006d38d8ad2c833384a32f5554
AguirreEze <aguirre.eze95@gmail.com>
2024-04-11 10:14:29 -0300

    feat: Characters Types

---
+++ types.ts
@@ -0,0 +1,30 @@
+export interface CharactersResponse {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: Character[];
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
