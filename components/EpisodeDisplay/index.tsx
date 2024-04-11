ff6bfe5dedcf1637f87d289e486403680e2aeb7b
AguirreEze <aguirre.eze95@gmail.com>
2024-04-11 12:02:37 -0300

    feat: base styles on EpisodesDisplay

--- components/EpisodeDisplay/index.tsx
+++ components/EpisodeDisplay/index.tsx
@@ -1,20 +1,26 @@
 "use client";
 
-import { CharacterSelection } from "@/context/characterSelection";
 import { useContext } from "react";
+import { CharacterSelection } from "@/context/characterSelection";
+
+import styles from "./styles.module.css";
 
 export default function EpisodesDisplay(): JSX.Element {
   const { data } = useContext(CharacterSelection);
 
   return (
-    <section>
+    <section className={styles.container}>
       <h2>
         {data.char1.name.length === 0 ? "Character #1" : data.char1.name} -
         Episodes
       </h2>
-      {data.char1.episode.map((url) => (
-        <h3 key={url}>{url}</h3>
-      ))}
+      <ul className={styles.list}>
+        {data.char1.episode.map((url) => (
+          <li key={url}>
+            <h3>{url}</h3>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
