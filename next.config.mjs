14b13b46c57535f17cd9306407e03ee5cf747dd5
AguirreEze <aguirre.eze95@gmail.com>
2024-04-11 14:20:05 -0300

    fix: deprecated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["rickandmortyapi.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+      },
+    ],
   },
 };
 
