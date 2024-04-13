2d9076ac9a725f150210b64b43a1f12030285118
AguirreEze <aguirre.eze95@gmail.com>
2024-04-13 16:50:02 -0300

    feat: validation on page value

--- components/CharactersDisplay/index.tsx
+++ components/CharactersDisplay/index.tsx
@@ -16,24 +16,36 @@ export default async function CharactersDisplay({
   param,
   character,
 }: Iprops): Promise<JSX.Element> {
+  function isPageValidValue(param: string | string[]): string {
+    // checks if param is a string
+    if (typeof param !== "string") return "1";
+    // check if the string only contains numbers
+    if (!/\d+/.test(param)) return "1";
+    // checks if is higher than 0
+    if (parseInt(param) < 1) return "1";
+    // checks if its higher than the max pages
+    if (parseInt(param) > 42) return "42";
+    return param;
+  }
+
   const data: CharactersResponse = await fetch(
-    `https://rickandmortyapi.com/api/character/?page=${page}`
+    `https://rickandmortyapi.com/api/character/?page=${isPageValidValue(page)}`
   ).then(async (res) => await res.json());
 
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>{CHARACTER_TITLE[character]}</h1>
       <nav className={styles.nav}>
-        <PaginationButton url={data.info.prev} param={param}>
+        <PaginationButton url={data.info?.prev} param={param}>
           {"<"}
         </PaginationButton>
-        <span>{page}</span>
-        <PaginationButton url={data.info.next} param={param}>
+        <span>{isPageValidValue(page)}</span>
+        <PaginationButton url={data.info?.next} param={param}>
           {">"}
         </PaginationButton>
       </nav>
       <ul className={styles.list}>
-        {data.results.map((character) => (
+        {data.results?.map((character) => (
           <li key={character.id}>
             <CharacterCard data={character} param={param} />
           </li>
